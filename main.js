295ce896c386b2c4d2a8a4b14a92198bab12652d
Rafael Fajardo <Rafael.A.Fajardo@gmail.com>
2019-12-12 13:51:11 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+/*
+ *
+ *  main.js should contain logic for selecting which game to play
+ *  it will use P5.JS and P5.Play.JS
+ *
+ */
+
+// declare global variables
+
+// preload
+function preload(){
+
+}
+
+// setup
+function setup(){
+
+}
+
+// draw
+function draw(){
+  // if not playing then show selection UI
+    // if selecting to play Crosser then show Crosser and hide others
+    // if selecting to play La Migra then show La Migra and hide others
+}
+
+// key or controller interface
+function keyTyped(){
+  if keyCode(71){ // keyCode for 'g' use for 'select' on controller
+    // toggle between Crosser and La Migra highlight selection
+    // can use a counter and modulo and set up an odd or even case
+    // counter+=
+    // if counter%(2) === 0 then do the following
+    // else do the following
+
+  }
+
+  if keyCode(72){ // keyCode for 'h' use for 'start' on controller
+    // start the highlighted selection
+  }
+
+}
+
+// function play( game ){}
