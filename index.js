9b2bd8d915ad4a40e92db3a65bd7c237743806cb
Chris <2659858+chrisgd@users.noreply.github.com>
2020-02-10 11:44:38 -0700

    set up static routing to allow node to serve pages under the public directory

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-
+// load up express
 const express = require('express')
 const app = express()
+
+// configure the port number
 const port = 8080
 
-app.get('/', (req, res) => res.send('hello world!'))
+// just set up simple static routing
+app.use('/', express.static(__dirname + '/public'))
 
-app.listen(port, () => console.log('example app listening on port ${port}!'))
+
+// and start listening
+app.listen(port, () => console.log('example app listening on port %s!', port))
